9fdefd5973d9874e0641afe3bcb21671901bafd9
Redskull-127 <76103614+Redskull-127@users.noreply.github.com>
2022-06-22 09:51:54 +0000

    Deploying to gh-pages from @ Redskull-127/OnTheGo@4835bf86c55417bd1d55c0a8489b0fc1c73eeb3f 🚀

---
+++ _next/static/WXrt0k8xOGEXOWS9DuaFI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/826-3d559536da311ada.js","static/css/21c565dc2d7921cb.css","static/chunks/pages/index-89e50796efa008bb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
